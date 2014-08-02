16375e23de1f5a3514bcee8bf0fbd473068f58ca
Josip Medved <jmedved@jmedved.com>
2014-08-02 00:39:58 -0700

    Added action setting.

--- killbom.py
+++ killbom.py
@@ -15,24 +15,24 @@ from mercurial import util
 
 def killbom(ui, repo, **opts):
     """Removes Unicode BOM."""
-    kb = KillBom(ui, repo, repo[None], opts)
-    return kb.process(True)
+    kb = KillBom(ui, repo, repo[None], opts, ['modify'])
+    return kb.process()
 
 def checkbom(ui, repo, **opts):
     """Checks for Unicode BOM."""
-    kb = KillBom(ui, repo, repo[None], opts)
-    return kb.process(False)
+    kb = KillBom(ui, repo, repo[None], opts, ['verify'])
+    return kb.process()
 
 
 def modify(ui, repo, **kwargs):
     ui.debug("killbom: modify hook\n")
-    kb = KillBom(ui, repo, repo[None], None)
-    return kb.process(True)
+    kb = KillBom(ui, repo, repo[None], None, ['modify', 'verify', 'ignore'])
+    return kb.process()
 
 def verify(ui, repo, **kwargs):
     ui.debug("killbom: verify hook\n")
-    kb = KillBom(ui, repo, repo[None], None)
-    return kb.process(False)
+    kb = KillBom(ui, repo, repo[None], None, ['verify', 'ignore'])
+    return kb.process()
 
 
 def reposetup(ui, repo):
@@ -43,21 +43,24 @@ def reposetup(ui, repo):
 cmdtable = {
     'killbom':  (
                  killbom,
-                 [('8', 'utf8only', False, 'removes only UTF-8 BOM signature')],
+                 [('8', 'utf8only', False, "removes only UTF-8 BOM signature")],
                  'hg killbom [options]'
                 ),
 
     'checkbom': (
                  checkbom,
-                 [('8', 'utf8only', False, 'checks for only UTF-8 BOM signature')],
+                 [('8', 'utf8only', False, "checks for only UTF-8 BOM signature")],
                  'hg checkbom [options]'
                 )
 }
 
+
 testedwith = '2.9.2 3.0.2'
 buglink = 'https://bitbucket.org/jmedved/hg-killbom/issues'
 
-################################################################################    
+
+################################################################################
+
 
 class KillBom:
     BOM_UTF8     = ('utf-8-sig', codecs.BOM_UTF8,     "utf-8")
@@ -67,21 +70,30 @@ class KillBom:
     BOM_UTF32_BE = ('utf-32_be', codecs.BOM_UTF32_BE, "utf-32be")
     BOM_ALL = [BOM_UTF8, BOM_UTF16_LE, BOM_UTF16_BE, BOM_UTF32_LE, BOM_UTF32_BE]
 
-    def __init__(self, ui, repo, ctx, opts):
+    def __init__(self, ui, repo, ctx, opts, validActions):
         self.ui = ui
-        self.repo = repo
         self.ctx = ctx
-        self.opts = opts
 
         if (opts == None):
             ui.debug("killbom: reading settings from hgrc\n")
+            action = ui.config('killbom', 'action', '');
             encodings = [encoding.lower() for encoding in ui.configlist('killbom', 'encodings', default=[])]
             maxsize = ui.config('killbom', 'maxsize', self.ui.config('tortoisehg', 'maxdiff', default='1024'));
 
         else: #check from command line
             ui.debug("killbom: reading settings from command line\n")
+            action = validActions[0];
             encodings = ['utf-8-sig'] if opts["utf8only"] else []
             maxsize = opts["maxsize"] if "maxsize" in opts else ''
+
+
+        action = action.lower()
+        self.action = validActions[0]
+        for validAction in validActions:
+            if validAction==action:
+                self.action = validAction
+                break
+        ui.note("killbom: action: {0}\n".format(self.action))
 
         self.encodings = []
         for encoding in encodings:
@@ -92,7 +104,6 @@ class KillBom:
             elif encoding=="utf-32-be" or encoding=="utf-32be" or encoding=="utf32be"                     : self.encodings.append(self.BOM_UTF32_BE)
             else: ui.warn("unknown encoding {0}!\n".format(encoding))
         if len(self.encodings) == 0: self.encodings = self.BOM_ALL
-                                                       
         ui.note("killbom: encodings:")
         [ui.note(" " + encoding[2]) for encoding in self.encodings]
         ui.note("\n")
@@ -106,9 +117,8 @@ class KillBom:
         ui.note("killbom: maxsize: {0} kb\n".format(self.maxsize / 1024))
 
             
-    def process(self, allowModify):
+    def process(self):    
         anyBomed = False
-
         files = self.ctx.files() if self.ctx else []
         self.ui.debug("killbom: checking {0} file(s)\n".format(len(files)))
 
@@ -141,10 +151,11 @@ class KillBom:
                     break
 
             if detectedEncoding==None:
-                self.ui.debug("killbom: skipping {0}: Unicode BOM not found\n".format(file))
+                if not self.action=='modify':
+                    self.ui.debug("killbom: skipping {0}: Unicode BOM not found\n".format(file))
                 continue
 
-            if allowModify:
+            if self.action=='modify':
                 self.ui.debug("killbom: removing {1} BOM in {0}\n".format(file, detectedEncoding[2]))
                 try:
                     with io.open(file, "rb") as f:
@@ -157,7 +168,7 @@ class KillBom:
                     #do it again otherwise utf-16 and utf-32 will have utf-8 bom present
                     raw = raw.decode("utf-8-sig")
                     raw = raw.encode("utf-8")
-         
+
                     newFile = file + ".killbom"
                     with io.open(newFile, "wb") as f:
                         f.write(raw)
@@ -172,4 +183,11 @@ class KillBom:
                 self.ui.note("killbom: skipped {1} BOM removal for {0}\n".format(file, detectedEncoding[2]))
                 anyBomed = True
 
-        return 1 if anyBomed else 0
+        if not anyBomed:
+            return 0
+        elif self.action=='ignore':
+            self.ui.debug("killbom: ignoring validation failure\n")
+            return 0
+        else:
+            return 1
+
